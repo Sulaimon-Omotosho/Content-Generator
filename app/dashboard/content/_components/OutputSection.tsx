963b77b538072b3bf2d65644e541e972f57881fe
Sulaimon Omotosho <susudef2u@gmail.com>
2025-03-20 17:15:48 +0100

    Feat: Finished Output of content section

--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import '@toast-ui/editor/dist/toastui-editor.css'
+import { Editor } from '@toast-ui/react-editor'
+import { Copy } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
 const OutputSection = () => {
-  return <div>OutputSection</div>
+  const editorRef: any = useRef()
+  return (
+    <div className='bg-white shadow-lg border rounded-lg'>
+      <div className='flex justify-between items-center p-5 '>
+        <h2 className='font-medium text-lg'>Your Result</h2>
+        <Button className='flex gap-2'>
+          <Copy /> Copy
+        </Button>
+      </div>
+      <Editor
+        ref={editorRef}
+        initialValue='Your Contents'
+        height='600px'
+        initialEditType='wysiwyg'
+        useCommandShortcut={true}
+        onChange={() =>
+          console.log(editorRef.current.getInstance().getMarkdown())
+        }
+      />
+    </div>
+  )
 }
 
 export default OutputSection
